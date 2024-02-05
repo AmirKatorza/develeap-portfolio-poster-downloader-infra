d9b4bc48fbd39efed31bafc2444fd6e8c38dd7af
Amir Katorza <amir.katorza@gmail.com>
2024-02-05 15:49:55 +0200

    Fixed variables issues

--- modules/vpc/3-subnets.tf
+++ modules/vpc/3-subnets.tf
@@ -4,22 +4,26 @@ resource "aws_subnet" "public_subnet_1a" {
   map_public_ip_on_launch = true
   availability_zone       = var.subnet_1a_az
 
-  tags = {
-    Name = var.subnet_1a_name
-    kubernetes.io/role/elb = "1"
-    kubernetes.io/cluster/${var.cluster_name} = "owned"
-  }
+  tags = merge(
+    {
+      "Name"                   = var.subnet_1a_name
+      "kubernetes.io/role/elb" = "1"
+    },
+    { "kubernetes.io/cluster/${var.cluster_name}" = "owned" }
+  )
 }
 
 resource "aws_subnet" "public_subnet_1b" {
-  vpc_id                  = aws_vpc.main_vpc.id 
+  vpc_id                  = aws_vpc.main_vpc.id
   cidr_block              = var.subnet_1b_cidr
   map_public_ip_on_launch = true
   availability_zone       = var.subnet_1b_az
 
-  tags = {
-    Name = var.subnet_1b_name
-    kubernetes.io/role/elb = "1"
-    kubernetes.io/cluster/${var.cluster_name} = "owned"
-  }
+  tags = merge(
+    {
+      "Name"                   = var.subnet_1b_name
+      "kubernetes.io/role/elb" = "1"
+    },
+    { "kubernetes.io/cluster/${var.cluster_name}" = "owned" }
+  )
 }
