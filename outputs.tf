9ea76f3faa13cd1526be9851b4a12428e5cbad97
Amir Katorza <amir.katorza@gmail.com>
2024-02-05 18:16:10 +0200

    Added meta outputs.tf file

---
+++ outputs.tf
@@ -0,0 +1,29 @@
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = module.vpc.vpc_id
+}
+
+output "public_subnet_1a_id" {
+  description = "The ID of the Subnet A"
+  value       = module.vpc.public_subnet_1a_id
+}
+
+output "public_subnet_1b_id" {
+  description = "The ID of Subnet B"
+  value       = module.vpc.public_subnet_1b_id
+}
+
+output "eks_cluster_id" {
+  description = "The ID of the EKS Cluster"
+  value       = module.eks.cluster_id
+}
+
+output "eks_node_group_role_arn" {
+  description = "The ARN of the IAM Role for the EKS Node Group"
+  value       = module.eks.node_group_role_arn
+}
+
+output "test_policy_arn" {
+  description = "The ARN of the test policy associated with the EKS cluster"
+  value       = module.eks.test_policy_arn
+}
